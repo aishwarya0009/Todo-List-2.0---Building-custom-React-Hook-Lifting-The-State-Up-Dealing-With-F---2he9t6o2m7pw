75022eb64da4ff20786389ef4a2ea4c8d71df080
aishwarya0009 <55315263+aishwarya0009@users.noreply.github.com>
2023-04-15 14:40:46 +0530

    finals commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,28 +1,45 @@
-import React, { useEffect, useState } from 'react'
-import SideNav from './SideNav';
-import MainSection from './MainSection';
-import Header from './Header';
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../styles/App.css';
-const App = () => {
-  const [state, setState] = useState("INBOX");
-  function kroChange(inp){
-     setState(inp);
-  }
-  return (
-    <div id="main">
-    <Header />
-      <div className='row'>
-        <div className='col-md-2'>
-          <SideNav change={kroChange}/>
-        </div>
-        <div className='col-md-10'>
-          <MainSection active = {state}/>
-        </div>
-      </div>
-    </div>
-  )
-}
+import React, { useState } from 'react'
 
+import SideNav from './SideNav';
+
+import MainSection from './MainSection';
+
+import Header from './Header';
+
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+import '../styles/App.css';
+
+const App = () => {
+
+const [active, setActive] = useState("INBOX");
+
+return (
+
+<div id="main">
+
+<Header />
+
+<div className='row'>
+
+<div className='col-md-2'>
+
+<SideNav change={setActive}/>
+
+</div>
+
+<div className='col-md-10'>
+
+<MainSection active = {active}/>
+
+</div>
+
+</div>
+
+</div>
+
+)
+
+}
 
 export default App;
--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -1,68 +1,133 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef } from "react";
 
 const Inbox = (props) => {
- 
-  const [newTask, setTask] = useState(false);
- 
-  const [ele, setele] = useState({number: 1, title: "haha", date: "1-1-2020"});
-  const [added, setadded] =useState(4);
-  const text = useRef(null);
-  const ddate = useRef(null);
-  function updateText(e){
-    e.preventDefault();
-    setadded(added+1);
-    setele({number: added, title: text.current.value, date: (new Date(ddate.current.value)).toLocaleDateString("en-US")})
-  }
-  useEffect(()=>{
-      if(added>4){
-        props.append(ele);
-      }
-    
-  },[ele])
-  console.log(added);
-  //console.log(ele);
-  
-  return (
-    
-    <div>
-      <h3>Inbox</h3>
-      {!newTask && (
-        <button className="new" onClick={()=>{setTask(true)}} id='add-new'>
-          +Add New
-        </button>
-      )}
-      {newTask && (
-        <form className="newtask-box">
-          <input type="text" id="title" ref={text}></input>
-          <div className="buttons">
-            <button className="new" id="add-list" onClick={(e)=>{updateText(e)}}>
-              Add Task
-            </button>
-            <button className="new" onClick={()=>{setTask(false)}}>
-              Cancel
-            </button>
-            <input
-              type="date"
-              ref={ddate}
-              defaultValue="2022-09-27"
-              id="date"
-            ></input>
-          </div>
-        </form>
-      )}
-      <div id="inbox">
-        {props.list.map((list) => {
-          return (
-            <div className="box" key={list.number}>
-              <div className="task">
-                {list.title} ({list.date})
-              </div>
-            </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+
+const [newTask, setNewTask] = useState(false);
+
+const titleRef = useRef(null);
+
+const calendarRef = useRef(null);
+
+const newTaskHandler = (e) => {
+
+setNewTask(true);
+
+};
+
+const addHandler = (e) => {
+
+e.preventDefault();
+
+if (titleRef.current.value === "") {
+
+window.alert("Task cannot be empty");
+
+return;
+
+}
+
+let newObj = {
+
+number: props.list.length + 1,
+
+title: titleRef.current.value,
+
+date: new Date(calendarRef.current.value),
+
+};
+
+props.append(newObj);
+
+setNewTask(false);
+
+};
+
+const cancelHandler = (e) => {
+
+setNewTask(false);
+
+};
+
+return (
+
+<div>
+
+<h3>Inbox</h3>
+
+{!newTask && (
+
+<button className="new" onClick={newTaskHandler} id='add-new'>
+
++Add New
+
+</button>
+
+)}
+
+{newTask && (
+
+<form className="newtask-box">
+
+<input type="text" id="title" ref={titleRef}></input>
+
+<div className="buttons">
+
+<button className="new" id="add-list" onClick={addHandler}>
+
+Add Task
+
+</button>
+
+<button className="new" onClick={cancelHandler}>
+
+Cancel
+
+</button>
+
+<input
+
+type="date"
+
+ref={calendarRef}
+
+defaultValue="2022-09-27"
+
+id="date"
+
+></input>
+
+</div>
+
+</form>
+
+)}
+
+<div id="inbox">
+
+{props.list.map((list) => {
+
+return (
+
+<div className="box" key={list.number}>
+
+<div className="task">
+
+{list.title} ({list.date.toLocaleDateString()})
+
+</div>
+
+</div>
+
+);
+
+})}
+
+</div>
+
+</div>
+
+);
+
 };
 
 export default Inbox;
